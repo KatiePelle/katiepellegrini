5b2733876ec1857cf3858967abf908ee8cb034dc
KatiePelle <112125504+KatiePelle@users.noreply.github.com>
2023-04-17 12:29:38 -0600

    final

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -1,74 +1,45 @@
-console.log("Question 1"); 
+let employees = [
+    {"firstName": "Sam", "department": "Tech", "designation": "Manager", "salary": 40000, "raiseEligible": "true"},
+    {"firstName": "Mary", "department": "Finance", "designation": "Trainee", "salary": 18500, "raiseEligible": "true"},
+    {"firstName": "Bill", "department": "HR", "designation": "Executive", "salary": 21000, "raiseEligible": "false"}
+]
 
-let employees = '{"employees": [' + 
-'{"firstname":"Sam" , "department":"Tech" , "designation":"Manager" , "salary": "40000" , "raise_eligible": "true"},' + 
-'{"firstname":"Mary" , "department":"Finance" , "designation":"Trainee" , "salary": "18500" , "raise_eligible": "true"},' + 
-'{"firstname":"Bill" , "department":"HR" , "designation":"Executive" , "salary": "21200" , "raise_eligible": "false"}' + 
-']}';
-
-const obj = JSON.parse(employees); 
-console.log("Current employees", employees); 
-console.log("Current employees JSON Object",obj);
-
-console.log("Question 2"); 
-let company = '{"companyName":"Tech Stars",' + '"website":"www.techstars.site",' + '"employees": ' + JSON.stringify(obj["employees"]) + '}';
-
-// console.log(company); 
-const obj2 = JSON.parse(company); 
-console.log("Current company",company);
-console.log("Current company JSON Object",obj2);
-//Problem 3 
-let employeelength = obj["employees"].length;
-obj["employees"] [employeelength] = {"first name": "Anna", "department": "Tech", "designation" : "Exceutive", "salary" : "25600" , "raise eligible" : "false"};
-obj2["employees"][employeelength] = {"first name": "Anna", "department": "Tech", "designation" : "Exceutive", "salary" : "25600" , "raise eligible" : "false"};
-console.log("Problem 3");
-console.log(obj);
-
-
-//Problem 4 
-console.log("Problem 4");
-let s = 0;
-for(let i =0; i < employeelength; i++)
-{
-    s = s + (parseInt(obj["employees"][i]["salary"]));
-
+let companyInfo = { "company":
+{"companuName": "Tech Stars", "website": "www.techstars.site","employees":employees}
 }
 
-console.log(s);
+console.log("Problem 1: ")
+console.log(employees)
+console.log("Problem 2: ")
+console.log(companyInfo.company)
 
-//Problem 5 
-console.log("Problem 5");
-for(let j = 0; j < employeelength; j++)
-{
-    if(obj["employees"][j]["raise_eligible"]  === "true")
-    {
-        obj["employees"][j]["salary"] = parseInt(obj["employees"][j]["salary"] * 0.1) + parseInt(obj["employees"][j]["salary"]);
+employees.push({"firstName" : "Anna", "department": "Tech", "designation": "Executive", "salary": 25600, "raiseEligible": "false" })
+console.log("Problem 3: ")
+console.log(employees)
+
+function total(){
+    let totalsalary = 0;
+    for (employee of employees) {
+        totalsalary +=employee.salary;
+    }
+    return totalsalary;
+}
+console.log("Problem 4: ")
+console.log(total ());
+
+for(let i = 0; i < 4; i++){
+    if (employee[i].raiseEligible === "true"){
+        employee[i].salary = employee[i].salary * 1.1;
     }
 }
-console.log("New employee list",obj);
+console.log("Problem 5: ")
+console.log(employees)
 
-//Problem 6
-console.log("Problem 6")
-for(let i =0; i < employeelength + 1; i++)
-{
-    if( obj["employees"][i]["firstname"] === "Sam")
-    {
-        obj2["employees"][i]["wfh"] = "true";
+for(let i = 0; i < 4; i++){
+    if (employees[i].firstName === "Anna" || employees[i].firstName ==="Sam") {
+        employees[i].wfh = true
+    } else {
+        employee[i].wfh = false
     }
-    if(i = 3)
-    {
-        obj2["employees"][i]["wfh"] = "true";
-    }
-    else
-    {
-        obj2["employees"][i]["wfh"] = "false"; 
-    }
-
-}
-console.log("New company list",obj2);
-
-
-
-
-
     
+}
