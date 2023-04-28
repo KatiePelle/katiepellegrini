af518dce5680c4f7b64295f805bb2f2bebdb6432
KatiePelle <112125504+KatiePelle@users.noreply.github.com>
2023-04-28 10:46:03 -0600

    wa14

--- js/Final/final.js
+++ js/Final/final.js
@@ -1,41 +1,44 @@
-const text = "The quick brown fox jumps over the lazy dog.";
+// Initialize variables
 let startTime, endTime;
+let numWords = 0;
+const wordsPerMinute = 200;
 
-document.getElementById("text").innerHTML = text;
-
+// Listen for the start button click event
 document.getElementById("start").addEventListener("click", function() {
-  startTime = new Date();
+  // Hide the start button and show the input area
   this.disabled = true;
-  document.getElementById("input").disabled = false;
-  document.getElementById("submit").disabled = false;
-  document.getElementById("reset").disabled = false;
+  document.getElementById("input-area").style.display = "block";
+
+  // Focus on the input field and start the timer
+  document.getElementById("input").focus();
+  startTime = new Date().getTime();
 });
 
-document.getElementById("submit").addEventListener("click", function() {
-  endTime = new Date();
-  const timeDiff = endTime - startTime;
-  const seconds = timeDiff / 1000;
-  const minutes = seconds / 60;
-  const words = text.split(" ");
-  const numWords = words.length;
-  const wpm = numWords / minutes;
+// Listen for the input field keyup event
+document.getElementById("input").addEventListener("keyup", function(event) {
+  if (event.key === " ") {
+    // Get the input value and split it into words
+    const input = this.value.trim();
+    const words = input.split(" ");
 
-  let resultPage;
-  if (wpm > 80) {
-    resultPage = "excellent.html";
-  } else if (wpm > 60) {
-    resultPage = "good.html";
-  } else if (wpm > 40) {
-    resultPage = "average.html";
-  } else {
-    resultPage = "poor.html";
+    // Count the number of words and calculate the elapsed time
+    numWords = words.length;
+    endTime = new Date().getTime();
+    const timeElapsed = endTime - startTime;
+    const seconds = timeElapsed / 1000;
+    const minutes = seconds / 60;
+
+    // Calculate the typing speed and redirect to the result page
+    const speed = Math.round(numWords / minutes);
+    const resultPage = `result.html?speed=${speed}`;
+    window.location.href = resultPage;
   }
-
-  sessionStorage.setItem("time", seconds.toFixed(2));
-  sessionStorage.setItem("wpm", wpm.toFixed(2));
-  window.location.href = resultPage;
 });
 
+// Listen for the reset button click event
 document.getElementById("reset").addEventListener("click", function() {
-  window.location.reload();
+  // Reset the input field and show the start button
+  document.getElementById("input").value = "";
+  document.getElementById("input-area").style.display = "none";
+  document.getElementById("start").disabled = false;
 });
