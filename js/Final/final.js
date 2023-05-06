b7d94532ff28b3190ff0de2905a07be3ac4d99ab
KatiePelle <112125504+KatiePelle@users.noreply.github.com>
2023-05-06 14:50:25 -0600

    Update final.js

--- js/Final/final.js
+++ js/Final/final.js
@@ -1,44 +1,52 @@
-// Initialize variables
-let startTime, endTime;
-let numWords = 0;
-const wordsPerMinute = 200;
+let count = 0;
+let timer = 10;
+let countDown;
 
-// Listen for the start button click event
-document.getElementById("start").addEventListener("click", function() {
-  // Hide the start button and show the input area
-  this.disabled = true;
-  document.getElementById("input-area").style.display = "block";
+function startGame() {
+  count = 0;
+  timer = 10;
+  document.getElementById("play-button").style.display = "none";
+  document.getElementById("click-button").style.display = "block";
+  document.getElementById("click-count").innerHTML = "0 clicks";
+  document.getElementById("results").style.display = "none";
+  startTimer();
+}
 
-  // Focus on the input field and start the timer
-  document.getElementById("input").focus();
-  startTime = new Date().getTime();
-});
+function incrementCount() {
+  count++;
+  document.getElementById("click-count").innerHTML = count + " clicks";
+}
 
-// Listen for the input field keyup event
-document.getElementById("input").addEventListener("keyup", function(event) {
-  if (event.key === " ") {
-    // Get the input value and split it into words
-    const input = this.value.trim();
-    const words = input.split(" ");
+function startTimer() {
+  countDown = setInterval(() => {
+    timer--;
+    if (timer < 0) {
+      clearInterval(countDown);
+      endGame();
+    }
+    document.getElementById("timer").innerHTML = timer;
+  }, 1000);
+}
 
-    // Count the number of words and calculate the elapsed time
-    numWords = words.length;
-    endTime = new Date().getTime();
-    const timeElapsed = endTime - startTime;
-    const seconds = timeElapsed / 1000;
-    const minutes = seconds / 60;
-
-    // Calculate the typing speed and redirect to the result page
-    const speed = Math.round(numWords / minutes);
-    const resultPage = `result.html?speed=${speed}`;
-    window.location.href = resultPage;
+function endGame() {
+  document.getElementById("click-button").style.display = "none";
+  document.getElementById("results").style.display = "block";
+  document.getElementById("score").innerHTML = count;
+  let rank;
+  if (count >= 50) {
+    rank = "Excellent!";
+  } else if (count >= 30) {
+    rank = "Average";
+  } else if (count >= 10) {
+    rank = "Poor";
+  } else {
+    rank = "Terrible";
   }
-});
+  document.getElementById("rank").innerHTML = rank;
+}
 
-// Listen for the reset button click event
-document.getElementById("reset").addEventListener("click", function() {
-  // Reset the input field and show the start button
-  document.getElementById("input").value = "";
-  document.getElementById("input-area").style.display = "none";
-  document.getElementById("start").disabled = false;
-});
+function resetGame() {
+  document.getElementById("play-button").style.display = "block";
+  document.getElementById("results").style.display = "none";
+}
+
